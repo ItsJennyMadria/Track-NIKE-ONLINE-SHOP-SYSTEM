31b24164c67be8891792b8c5973498c7df4573a9
Yuuki <meyklucero@gmail.com>
2026-05-05 01:09:51 +0800

    Finish Styling

--- index.php
+++ index.php
@@ -17,20 +17,28 @@ if ($username == "admin" && $password == "1234") {
 
 <head>
     <title>LOGIN FORM</title>
+    <link rel="stylesheet" href="register.css">
 </head>
 
 <body>
-    <h2>Login Form</h2>
-    <form method="POST" action="">
-        <label>Username:</label><br>
-        <input type="text" name="username" required><br><br>
+    <div class="form-container">
+        <div class="shop-title">NIKE ONLINE SHOP</div>
+        <div class="form-title">LOGIN</div>
 
-        <label>Password:</label><br>
-        <input type="password" name="password" required><br><br>
+        <form method="POST" action="">
+            <label>Username:</label><br>
+            <input type="text" name="username" required>
 
-        <button type="submit">Login</button>
-    </form>
-    <p><?php echo $message; ?></p>
+            <label>Password:</label><br>
+            <input type="password" name="password" required>
+
+            <button type="submit">Log in</button>
+        </form>
+        <p><?php echo $message; ?></p>
+
+        <div class="login-text">
+            Don't have an account? <a href="register.php">Register</a>
+        </div>
 </body>
 
 </html>
