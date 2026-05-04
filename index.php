951cd613c7398a035533ef89cde756e3bd72f976
Yuuki <meyklucero@gmail.com>
2026-05-05 00:30:03 +0800

    Added changes to log in form

--- index.php
+++ index.php
@@ -1,15 +1,34 @@
+<?php
+$message = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") { 
+    $username = $_POST["username"]; 
+    $password = $_POST["password"]; 
+    
+if ($username == "admin" && $password == "1234") { 
+    $message = "Login Successful!"; 
+} else { 
+    $message = "Invalid Username or Password."; 
+    } 
+} 
+?>
+
 <!DOCTYPE html>
 <html>
 
 <head>
-    <title>Login Form</title>
+    <title>LOGIN FORM</title>
 </head>
 
 <body>
     <h2>Login Form</h2>
-    <form method="POST" action=""> <label>Username:</label><br> <input type="text" name="username" required><br><br>
-        <label>Password:</label><br> <input type="password" name="password" required><br><br> <button
-            type="submit">Login</button>
+    <form method="POST" action="">
+        <label>Username:</label><br>
+        <input type="text" name="username" required><br><br>
+
+        <label>Password:</label><br>
+        <input type="password" name="password" required><br><br>
+
+        <button type="submit">Login</button>
     </form>
     <p><?php echo $message; ?></p>
 </body>
