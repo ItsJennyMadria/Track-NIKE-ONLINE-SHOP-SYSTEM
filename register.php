0a2e89a1cdd57c584134b17a403ea1315e23db0c
Yuuki <meyklucero@gmail.com>
2026-05-04 19:29:23 +0800

    Added form control

--- register.php
+++ register.php
@@ -18,25 +18,29 @@
     <div>
         <form action="register.php" method="POST">
             <div class="container"></div>
-            <h1>REGISTRATION</h1>
-            <p>Fill up the form with correct values.</p>
-            <label for="firstname"><b>First Name</b></label>
-            <input type="text" name="firstname" required>
 
-            <label for="lastname"><b>Last Name</b></label>
-            <input type="text" name="lastname" required>
+            <div class="row">
+                <div class="col-sm-3">
+                    <h1>REGISTRATION</h1>
+                    <p>Fill up the form with correct values.</p>
+                    <label for="firstname"><b>First Name</b></label>
+                    <input class="form-control" type="text" name="firstname" required>
 
-            <label for="email"><b>Email Address</b></label>
-            <input type="email" name="email" required>
+                    <label for="lastname"><b>Last Name</b></label>
+                    <input class="form-control" type="text" name="lastname" required>
 
-            <label for="phonenumber"><b>Phone Number</b></label>
-            <input type="text" name="phonenumber" required>
+                    <label for="email"><b>Email Address</b></label>
+                    <input class="form-control" type="email" name="email" required>
 
-            <label for="password"><b>Password</b></label>
-            <input type="password" name="password" required>
+                    <label for="phonenumber"><b>Phone Number</b></label>
+                    <input class="form-control" type="text" name="phonenumber" required>
 
-            <input type="submit" name="create" value="Sign Up">
+                    <label for="password"><b>Password</b></label>
+                    <input class="form-control" type="password" name="password" required>
 
+                    <input type="submit" name="create" value="Sign Up">
+                </div>
+            </div>
         </form>
 
     </div>
